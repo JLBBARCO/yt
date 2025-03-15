6fa182e76687047d853d6705891beae3b11687c2
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2025-03-15 11:35:36 -0300

    v0.003

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// Bibliotecas
+
+// Tags
+url = document.getElementById('url');
+mp3 = document.getElementById("mp3");
+mp4 = document.getElementById("mp4");
+resume = document.getElementById("resume");
+
+// Funções
+if (mp3 == True) {} else if (mp4 == True) {} else if (resume == True) {};
+
 // Ano
 var data = new Date();
 var atual = data.getFullYear();
